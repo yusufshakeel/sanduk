b47714f3954759e8577b846ac704fc5f1cce74d2
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2021-07-04 13:41:46 +0530

    main browser window code

--- src/main-process/main.js
+++ src/main-process/main.js
@@ -5,11 +5,29 @@ let mainWindow;
 const createMainWindow = () => {
     mainWindow = new BrowserWindow({
         minWidth: 400,
-        minHeight: 400
+        minHeight: 400,
+        backgroundColor: '#fff'
     });
     mainWindow.loadFile(__dirname + '/main.html');
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
 };
 
 app.whenReady().then(() => {
     createMainWindow();
+});
+
+// Quit the app when all windows are closed. Not for macOS.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+// When app icon is clicked and app is running then recreate BrowserWindow (macOS)
+app.on('activate', () => {
+    if (mainWindow === null) {
+        createMainWindow();
+    }
 });
