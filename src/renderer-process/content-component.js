07590229de147cb1890ca9d976e196e9c31ae1cc
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2021-07-12 23:52:42 +0530

    uuid

--- src/renderer-process/content-component.js
+++ src/renderer-process/content-component.js
@@ -1,7 +1,51 @@
 'use strict';
+const UUIDToolComponent = require('./tools/uuid-tool-component');
+
+const uuidToolComponent = new UUIDToolComponent();
 
 module.exports = async function contentComponent() {
-  const html = `html`;
+  const html = `<div class="container-fluid p-10">
+    <div class="row">
+      <div class="col-12" id="main-content">
+        <div id="base64-encoder-decoder-content-wrapper"
+             data-sanduk-tool-isActive="false"
+             data-sanduk-tool-name="base64-encoder-decoder"
+             class="sandook-tool-content-wrapper d-none">Base64 Encoder Decoder
+        </div>
+        <div id="epoch-content-wrapper"
+             data-sanduk-tool-isActive="false"
+             data-sanduk-tool-name="epoch"
+             class="sandook-tool-content-wrapper d-none">epoch
+        </div>
+        <div id="json-formatter-content-wrapper"
+             data-sanduk-tool-isActive="false"
+             data-sanduk-tool-name="json-formatter"
+             class="sandook-tool-content-wrapper d-none">json formatter
+        </div>
+        <div id="jwt-decoder-content-wrapper"
+             data-sanduk-tool-isActive="false"
+             data-sanduk-tool-name="jwt-decoder"
+             class="sandook-tool-content-wrapper d-none">jwt decoder
+        </div>
+        <div id="uuid-content-wrapper"
+             data-sanduk-tool-isActive="false"
+             data-sanduk-tool-name="uuid"
+             class="sandook-tool-content-wrapper d-none">
+          ${uuidToolComponent.getUUIDV4Html()}
+          <hr class="my-20 border-bottom">
+          ${uuidToolComponent.getUUIDV5Html()}
+        </div>
+        <div id="xml-formatter-content-wrapper"
+             data-sanduk-tool-isActive="false"
+             data-sanduk-tool-name="xml-formatter"
+             class="sandook-tool-content-wrapper d-none">xml formatter
+        </div>
+      </div>
+    </div>
+  </div>`;
 
   document.getElementById('content-wrapper').innerHTML = html;
+
+  uuidToolComponent.initUUIDV4();
+  uuidToolComponent.initUUIDV5();
 };
--- src/renderer-process/tools/uuid-tool-component.js
+++ src/renderer-process/tools/uuid-tool-component.js
@@ -1,3 +1,129 @@
 'use strict';
+const { clipboard } = require('electron');
+const { v4: uuidV4, v5: uuidV5 } = require('uuid');
 
-module.exports = async function uuidToolComponent() {};
+module.exports = function UUIDToolComponent() {
+  this.getUUIDV4Html = () => {
+    return `<!-- uuid v4 -->
+  <div class="row mb-20">
+    <div class="col-12 text-center">
+      <p class="font-size-20 my-0">UUID v4</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-12">
+      <div class="form-group">
+        <input class="form-control text-center text-monospace font-size-18" id="uuid-v4-output" readonly>
+      </div>
+      <button id="generate-uuid-v4-btn" class="btn btn-rounded btn-primary sanduk-tool-btn">Generate</button>
+      <button id="copy-uuid-v4-btn" class="btn btn-rounded btn-secondary sanduk-tool-btn">Copy</button>
+      <button id="clear-uuid-v4-btn" class="btn btn-rounded btn-default sanduk-tool-btn float-right">Clear</button>
+    </div>
+  </div>`;
+  };
+
+  this.getUUIDV5Html = () => {
+    return `<!-- uuid v5 -->
+  <div class="row my-20">
+    <div class="col-12 text-center">
+      <p class="font-size-20 my-0">UUID v5</p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-12">
+      <div class="form-group">
+        <label for="uuid-v5-name">String</label>
+        <input class="form-control" id="uuid-v5-name">
+      </div>
+      <div class="form-group">
+        <label for="uuid-v5-namespace">Namespace</label>
+        <input class="form-control" id="uuid-v5-namespace">
+        <div id="uuid-v5-namespace-message"></div>
+      </div>
+      <div class="form-group">
+        <label for="uuid-v5-output">UUID v5</label>
+        <input class="form-control text-center text-monospace font-size-18" id="uuid-v5-output" readonly>
+      </div>
+      <button id="generate-uuid-v5-btn" class="btn btn-rounded btn-primary sanduk-tool-btn">Generate</button>
+      <button id="copy-uuid-v5-btn" class="btn btn-rounded btn-secondary sanduk-tool-btn">Copy</button>
+      <button id="clear-uuid-v5-btn" class="btn btn-rounded btn-default sanduk-tool-btn float-right">Clear</button>
+    </div>
+  </div>`;
+  };
+
+  this.initUUIDV4 = () => {
+    const btnGenerateUUIDV4 = document.getElementById('generate-uuid-v4-btn');
+    const btnCopyUUIDV4 = document.getElementById('copy-uuid-v4-btn');
+    const uuidV4Output = document.getElementById('uuid-v4-output');
+    const btnClearUUIDV4 = document.getElementById('clear-uuid-v4-btn');
+
+    btnGenerateUUIDV4.addEventListener('click', () => {
+      uuidV4Output.value = uuidV4();
+    });
+
+    btnCopyUUIDV4.addEventListener('click', () => {
+      if (!uuidV4Output.value.trim().length) {
+        return;
+      }
+      btnCopyUUIDV4.innerText = 'Copied!';
+      clipboard.writeText(uuidV4Output.value);
+      setTimeout(() => {
+        btnCopyUUIDV4.innerText = 'Copy';
+      }, 200);
+    });
+
+    btnClearUUIDV4.addEventListener('click', () => {
+      uuidV4Output.value = '';
+    });
+  };
+
+  this.initUUIDV5 = () => {
+    const uuidV5Name = document.getElementById('uuid-v5-name');
+    const uuidV5Namespace = document.getElementById('uuid-v5-namespace');
+    const uuidV5NamespaceMessage = document.getElementById('uuid-v5-namespace-message');
+    const btnGenerateUUIDV5 = document.getElementById('generate-uuid-v5-btn');
+    const btnCopyUUIDV5 = document.getElementById('copy-uuid-v5-btn');
+    const btnClearUUIDV5 = document.getElementById('clear-uuid-v5-btn');
+    const uuidV5Output = document.getElementById('uuid-v5-output');
+
+    function hideError() {
+      uuidV5NamespaceMessage.innerHTML = '';
+    }
+
+    function showError(message) {
+      uuidV5NamespaceMessage.innerHTML = `<div class="alert alert-danger" role="alert">${message}</div>`;
+      setTimeout(hideError, 5000);
+    }
+
+    btnGenerateUUIDV5.addEventListener('click', () => {
+      try {
+        hideError();
+        if (!uuidV5Namespace.value.trim().length) {
+          showError('Namespace required.');
+          return;
+        }
+        uuidV5Output.value = uuidV5(uuidV5Name.value, uuidV5Namespace.value);
+      } catch (e) {
+        showError(e.message);
+      }
+    });
+
+    btnCopyUUIDV5.addEventListener('click', () => {
+      if (!uuidV5Output.value.trim().length) {
+        return;
+      }
+      btnCopyUUIDV5.innerText = 'Copied!';
+      clipboard.writeText(uuidV5Output.value);
+      setTimeout(() => {
+        btnCopyUUIDV5.innerText = 'Copy';
+      }, 200);
+    });
+
+    btnClearUUIDV5.addEventListener('click', () => {
+      hideError();
+      uuidV5Output.value = '';
+      uuidV5Name.value = '';
+      uuidV5Namespace.value = '';
+    });
+  };
+};
