44eb1529df98eda28835881c35dfb1fc471bccfb
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2021-09-21 21:59:59 +0530

    added jwt decoder logic

--- src/components/tools/ToolJWTDecoder.js
+++ src/components/tools/ToolJWTDecoder.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AceEditor from 'react-ace';
-import 'ace-builds/src-noconflict/mode-java';
-import 'ace-builds/src-noconflict/theme-github';
+import 'ace-builds/src-noconflict/mode-text';
+import 'ace-builds/src-noconflict/mode-json5';
+import 'ace-builds/src-noconflict/theme-monokai';
 import 'ace-builds/src-noconflict/ext-searchbox';
-import { Button, Grid, Typography } from '@mui/material';
+import jwtDecode from 'jwt-decode';
+import { Alert, AlertTitle, Button, Grid, Typography } from '@mui/material';
 
 function ToolJWTDecoder() {
-  function onChange(newValue) {
-    // eslint-disable-next-line no-console
-    console.log('change', newValue);
-  }
+  const [jwtInput, setJwtInput] = useState('');
+  const [jwtHeader, setJwtHeader] = useState('');
+  const [jwtPayload, setJwtPayload] = useState('');
+
+  const [message, setMessage] = useState(<React.Fragment />);
+
+  const handleDecodeJwt = () => {
+    try {
+      setMessage('');
+
+      const input = jwtInput.trim();
+      if (!input.length) {
+        return;
+      }
+
+      const decodedHeader = jwtDecode(input, { header: true });
+      const decodedJWT = jwtDecode(input);
+
+      setJwtHeader(JSON.stringify(decodedHeader, null, 2));
+      setJwtPayload(JSON.stringify(decodedJWT, null, 2));
+    } catch (e) {
+      setMessage(
+        <Alert variant="filled" severity="error">
+          <AlertTitle>Error</AlertTitle>
+          {e.message}
+        </Alert>
+      );
+    }
+  };
+
+  const clearJwtInputFields = () => {
+    setJwtInput('');
+    setJwtHeader('');
+    setJwtPayload('');
+    setMessage('');
+  };
 
   return (
     <Grid container>
@@ -24,9 +58,11 @@ function ToolJWTDecoder() {
         </Typography>
         <div id="jwtEditorContainer" sx={{ mb: 5 }}>
           <AceEditor
-            mode="java"
-            theme="github"
-            onChange={onChange}
+            mode="text"
+            theme="monokai"
+            value={jwtInput}
+            onChange={e => setJwtInput(e)}
+            wrapEnabled={true}
             name="jwtEditor"
             style={{ width: '100%', height: '200px', marginBottom: '40px' }}
             setOptions={{
@@ -37,14 +73,27 @@ function ToolJWTDecoder() {
           />
         </div>
         <div style={{ marginBottom: '40px' }}>
-          <Button id="decodeBtn" variant="contained" color="primary" size="large" sx={{ mr: 2 }}>
+          <Button
+            id="decodeBtn"
+            variant="contained"
+            color="primary"
+            size="large"
+            sx={{ mr: 2 }}
+            onClick={handleDecodeJwt}
+          >
             Decode
           </Button>
-          <Button id="clearBtn" variant="text" color="info" size="large">
+          <Button
+            id="clearBtn"
+            variant="text"
+            color="info"
+            size="large"
+            onClick={clearJwtInputFields}
+          >
             Clear
           </Button>
         </div>
-        <div id="messageContainer" />
+        <div id="messageContainer">{message}</div>
       </Grid>
       <Grid item xs={12} sm={6} sx={{ p: 1 }}>
         <Typography variant="h6" sx={{ mb: 1 }}>
@@ -52,8 +101,9 @@ function ToolJWTDecoder() {
         </Typography>
         <div id="jwtHeaderEditorContainer" sx={{ mb: 5 }}>
           <AceEditor
-            mode="java"
-            theme="github"
+            mode="json5"
+            theme="monokai"
+            value={jwtHeader}
             name="jwtHeaderEditor"
             style={{ width: '100%', height: '400px', marginBottom: '40px' }}
             setOptions={{
@@ -70,8 +120,9 @@ function ToolJWTDecoder() {
         </Typography>
         <div id="jwtPayloadEditorContainer" sx={{ mb: 5 }}>
           <AceEditor
-            mode="java"
-            theme="github"
+            mode="json5"
+            theme="monokai"
+            value={jwtPayload}
             name="jwtPayloadEditor"
             style={{ width: '100%', height: '400px', marginBottom: '40px' }}
             setOptions={{
