059f9951c41f74e1408c78a0484968be0802bfa1
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2021-09-25 17:44:38 +0530

    test

--- src/components/helpers/DisappearingComponent.test.js
+++ src/components/helpers/DisappearingComponent.test.js
@@ -12,3 +12,25 @@ test('Render', async () => {
     expect(screen.queryByText('Hello World')).not.toBeInTheDocument();
   });
 });
+
+test('Re-render', async () => {
+  render(
+    <DisappearingComponent timeout={1000}>
+      <h1>Hello World</h1>
+    </DisappearingComponent>
+  );
+  expect(screen.getByText('Hello World')).toBeInTheDocument();
+  await waitFor(() => {
+    expect(screen.queryByText('Hello World')).not.toBeInTheDocument();
+  });
+
+  render(
+    <DisappearingComponent timeout={1000}>
+      <h1>Hello World 2</h1>
+    </DisappearingComponent>
+  );
+  expect(screen.getByText('Hello World 2')).toBeInTheDocument();
+  await waitFor(() => {
+    expect(screen.queryByText('Hello World 2')).not.toBeInTheDocument();
+  });
+});
