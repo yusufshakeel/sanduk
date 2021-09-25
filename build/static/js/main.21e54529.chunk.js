0129e7e27016e3c1aa602c82095f2f56378c692c
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2021-09-26 01:25:11 +0530

    build folder removed

--- build/static/js/main.21e54529.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpsanduk=this.webpackJsonpsanduk||[]).push([[0],{9:function(n,t,e){"use strict";e.r(t);var c=e(1),i=e.n(c),o=e(3),r=e.n(o),s=e(0);var u=function(){return Object(s.jsx)("h1",{children:"Hello Sanduk!"})},a=function(n){n&&n instanceof Function&&e.e(3).then(e.bind(null,10)).then((function(t){var e=t.getCLS,c=t.getFID,i=t.getFCP,o=t.getLCP,r=t.getTTFB;e(n),c(n),i(n),o(n),r(n)}))};r.a.render(Object(s.jsx)(i.a.StrictMode,{children:Object(s.jsx)(u,{})}),document.getElementById("root")),a()}},[[9,1,2]]]);
-//# sourceMappingURL=main.21e54529.chunk.js.map
