c5173a7ea06a6febb2215dd62c7237a99e4b8c2c
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2021-09-25 23:30:40 +0530

    test fix

--- src/App.test.js
+++
@@ -1,7 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('Launch App', () => {
-  render(<App />);
-  expect(screen.getByText('sanduk')).toBeInTheDocument();
-});
--- src/components/helpers/EditorStatusBar.test.js
+++ src/components/helpers/EditorStatusBar.test.js
@@ -3,8 +3,11 @@ import EditorStatusBar from './EditorStatusBar';
 
 test('Render', () => {
   render(<EditorStatusBar line={10} column={20} fontSize={16} tabSize={2} />);
-  const lineColumn = screen.getByText('Ln: 10 Col: 20');
-  expect(lineColumn).toBeInTheDocument();
+  const line = screen.getByText('Ln: 10');
+  expect(line).toBeInTheDocument();
+
+  const column = screen.getByText('Col: 20');
+  expect(column).toBeInTheDocument();
 
   const fontSize = screen.getByText('Font 16px');
   expect(fontSize).toBeInTheDocument();
