890ef81bfb374946bb04b039af51ad0fb7e6e90e
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2022-02-22 20:30:10 +0530

    update

--- src/renderer-process/app.js
+++ src/renderer-process/app.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const tools = require('../renderer-process/tools');
+const packageJson = require('../../package.json');
 
 window.onload = () => {
+  const { version } = packageJson;
+  document.getElementById('sanduk-project-version').innerText = version;
   tools();
 };
