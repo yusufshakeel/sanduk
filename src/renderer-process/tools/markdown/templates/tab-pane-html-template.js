ad6962d401dd21e4d91215450aeab22c284fe12e
Yusuf Shakeel <yusufshakeel.in@gmail.com>
2022-02-26 22:16:32 +0530

    ui refactoring - fixing spacing

--- src/renderer-process/tools/markdown/templates/tab-pane-html-template.js
+++ src/renderer-process/tools/markdown/templates/tab-pane-html-template.js
@@ -14,8 +14,8 @@ module.exports = function tabPaneHtmlTemplate({
   addActiveClass = false
 }) {
   const showActiveClassName = addActiveClass ? 'show active' : '';
-  const editorHeight = 'calc(100vh - 250px)';
-  const iframeHeight = 'calc(100vh - 250px)';
+  const editorHeight = 'calc(100vh - 190px)';
+  const iframeHeight = 'calc(100vh - 160px)';
   const markdownEditorFooterHtml = new EditorFooterBuilder({ prefix: prefixForMarkdown, id })
     .withRowColumnPosition()
     .build();
@@ -44,7 +44,7 @@ module.exports = function tabPaneHtmlTemplate({
   };
 
   return `<div class="tab-pane ${showActiveClassName}" id="${prefix}-tab-${id}-content" role="tabpanel" aria-labelledby="tab-${id}">
-  <div class="container-fluid">
+  <div class="container-fluid" style="padding: 0 12px 0;">
     <div class="row">
       <div class="col-md-12 col-lg-6 p-0" id="${prefix}-tab-${id}-markdown-column"
         data-viewEditorOnly="col-md-12 col-lg-12 p-0"
